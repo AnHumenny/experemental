4c3328c165517e29dece1fd22fd3234297c7559a
AnHumenny <an.gumenniy@gmail.com>
2024-09-10 10:29:54 +0300

    Update config -> TOKEN.py

--- 21_08_2024_asyncio_aiorgam_sqlalchemy_request/bot_current/bot.py
+++ 21_08_2024_asyncio_aiorgam_sqlalchemy_request/bot_current/bot.py
@@ -2,9 +2,8 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from lists import API_TOKEN
+from config import API_TOKEN
 from handlers import router
-
 
 
 async def main():
