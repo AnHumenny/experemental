4bb1258b231f31829db8ee70a7d0742071bd1116
AnHumenny <an.gumenniy@gmail.com>
2024-09-10 10:31:05 +0300

    Add files via upload

---
+++ 21_08_2024_asyncio_aiorgam_sqlalchemy_request/bot_current/config.py
@@ -0,0 +1,16 @@
+API_TOKEN = 'TOKEN'
+abs_path = "PATH_TO_FOLDER_IMAGE"
+check_list = ["USD", "EUR", "RUB", "CNY"]
+
+list_url = [
+    "https://myfin.by/currency/torgi-na-bvfb/kurs-dollara",
+    "https://myfin.by/currency/torgi-na-bvfb/kurs-euro",
+    "https://myfin.by/currency/torgi-na-bvfb/kurs-rublya",
+    "https://myfin.by/currency/torgi-na-bvfb/kurs-cny"
+]
+
+
+host="host"
+user="user"
+password="password"
+database="database"
