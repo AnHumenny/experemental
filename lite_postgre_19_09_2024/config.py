4d1f3b30f64a50be60c6c39a805e7648921ae5b1
AnHumenny <an.gumenniy@gmail.com>
2024-09-18 18:54:59 +0300

    Update config.py

--- lite_postgre_19_09_2024/config.py
+++ lite_postgre_19_09_2024/config.py
@@ -1,6 +1,6 @@
 # Параметры подключения к PostgreSQL
 host = "localhost"
-database = "dj_start"
-user = "admen"
-password = "1qazxcde3"
+database = "hosthame"
+user = "user"
+password = "password"
 
