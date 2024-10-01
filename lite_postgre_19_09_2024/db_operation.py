ff13b9a64e343d714040f319b61993e2fe565f4f
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-10-01 12:26:41 +0300

    Update db_operation.py
    added table deletion, fixed a bug with postgresql closure

--- lite_postgre_19_09_2024/db_operation.py
+++ lite_postgre_19_09_2024/db_operation.py
@@ -2,115 +2,126 @@ import psycopg2
 from config import host, password, user
 
 def create_database(dtb):
+    # Подключение к PostgreSQL
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database="postgres"
+    )
     try:
-        # Подключение к PostgreSQL
-        conn = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database="postgres"
-        )
-
-        # Проверка наличия базы данных
         with conn.cursor() as cursor:
             conn.autocommit = True
             sql = f"CREATE DATABASE {dtb}"
             cursor.execute(sql)
             print(f"База {dtb} данных успешно создана")
-        conn.close()
     except (Exception, psycopg2.Error) as error:
         print(f"Ошибка при работе с PostgreSQL: {error}")
-    return
-
+        return
+    finally:
+        conn.commit()
+        conn.close()
 
 def view_database():
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database="postgres"
+    )
+    conn.autocommit = True
+    sql = f"SELECT datname FROM pg_database"
     try:
-        conn = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database="postgres"
-        )
         with conn.cursor() as cursor:
-            conn.autocommit = True
-            sql = f"SELECT datname FROM pg_database"
             cursor.execute(sql)
             result = cursor.fetchall()
-        conn.close()
         return result
     except (Exception, psycopg2.Error) as error:
         print(f"Ошибка при работе с PostgreSQL: {error}")
+    finally:
+        conn.commit()
+        conn.close()
 
 
 def drop_database(dtb):
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database="postgres"
+    )
+    conn.autocommit = True
     try:
-        conn = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database="postgres"
-        )
         with conn.cursor() as cursor:
-            conn.autocommit = True
             sql = f"DROP DATABASE {dtb}"
             cursor.execute(sql)
             result = cursor.fetchall()
-        conn.close()
         return result
     except (Exception, psycopg2.Error) as error:
-       print(f"Ошибка при работе с PostgreSQL: {error}база данных {dtb} отсутствует")
-    return
-
+       print(f"\nОшибка при работе с PostgreSQL: {error}база данных {dtb} отсутствует")
+       return
+    finally:
+        conn.commit()
+        conn.close()
 
 def view_table_database(dtb):
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=dtb
+    )
+    conn.autocommit = True
+    sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
     try:
-        conn = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=dtb
-        )
         with conn.cursor() as cursor:
-            conn.autocommit = True
-            sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
             cursor.execute(sql)
             result = cursor.fetchall()
-        conn.close()
         return result
     except (Exception, psycopg2.Error) as error:
-       print(f"Ошибка при работе с PostgreSQL: {error}база данных {dtb} отсутствует")
-    return
+        print(f"\nОшибка при работе с PostgreSQL: {error}база данных {dtb} отсутствует")
+        return
+    finally:
+        conn.commit()
+        conn.close()
 
 def view_structure(dtb, table_name):
-    try:
-        conn = psycopg2.connect(
-            host=host,
-            database=dtb,
-            user=user,
-            password=password
-        )
-        cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM {table_name} LIMIT 1")
-        fields = [column[0] for column in cursor.description]
-        conn.close()
-        return fields
-    except (Exception, psycopg2.Error) as error:
-        print(f"Ошибка при работе с PostgreSQL: {error} данные не найдены")
-
-def view_role(dtb, usr):
     conn = psycopg2.connect(
         host=host,
         database=dtb,
-        user=usr,
+        user=user,
         password=password
     )
     cursor = conn.cursor()
-    # Получение списка пользователей и их прав
-    cursor.execute(
-        "SELECT rolname, rolcanlogin, rolsuper, rolinherit, rolcreaterole, rolcreatedb, rolbypassrls, rolconnlimit FROM pg_roles")
-    users = cursor.fetchall()
-    conn.close()
-    return users
+    try:
+        cursor.execute(f"SELECT * FROM {table_name} LIMIT 1")
+        fields = [column[0] for column in cursor.description]
+        return fields
+    except (Exception, psycopg2.Error) as error:
+        print(f"Ошибка при работе с PostgreSQL: {error} данные не найдены")
+    finally:
+        conn.commit()
+        conn.close()
+
+
+def view_role(dtb):
+    conn = psycopg2.connect(
+        host=host,
+        database=dtb,
+        user=user,
+        password=password
+    )
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            "SELECT rolname, rolcanlogin, rolsuper, rolinherit, rolcreaterole, rolcreatedb, rolbypassrls, rolconnlimit FROM pg_roles")
+        users = cursor.fetchall()
+        return users
+    except (Exception, psycopg2.Error) as error:
+        print(f"Ошибка при работе с PostgreSQL: {error} данные не найдены")
+    finally:
+        conn.commit()
+        conn.close()
 
 
 def view_user():
@@ -122,7 +133,30 @@ def view_user():
     )
     cur = conn.cursor()
     cur.execute("SELECT usename FROM pg_user")
-    users = cur.fetchall()
-    cur.close()
-    conn.close()
-    return users
+    try:
+        users = cur.fetchall()
+        return users
+    except (Exception, psycopg2.Error) as error:
+        print(f"Ошибка при работе с PostgreSQL: {error} пользователи не найдены")
+    finally:
+        conn.commit()
+        conn.close()
+
+
+def drop_table(dtb, table):
+    conn = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=dtb
+    )
+    cur = conn.cursor()
+    try:
+        cur.execute(f'DROP TABLE IF EXISTS {table};')
+        return f'Таблица {table} успешно удалена'
+    except (Exception, psycopg2.Error) as error:
+       return f"Ошибка при работе с PostgreSQL: {error}база данных {dtb} отсутствует"
+    finally:
+        cur.close()
+        conn.commit()
+        conn.close()
