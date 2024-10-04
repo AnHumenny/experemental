ac9bb316878e6a2061cd2e0d5d01886682a06585
Andrei Gumenniy <an.gumenniy@gmail.com>
2024-10-04 10:44:02 +0300

    Update main.py
    update to class

--- lite_postgre_19_09_2024/main.py
+++ lite_postgre_19_09_2024/main.py
@@ -1,19 +1,26 @@
-from db_operation import (create_database, view_database, drop_database, view_table_database,
-                          view_structure, view_role, view_user, drop_table)
 from create_tables import create_table
+import db_operation
+import config
 
-if __name__ == "__main__":
-    while True:
+class Operation:
+    def __init__(self):
+        self.dtbs = db_operation.Pg(host=config.host, port=config.port, user=config.user,
+                          password=config.password, database=config.database)
+
         print("-" * 130, "\n",
-              "create_DB - создать базу | "
-              "drop_DB - удалить базу | "
-              "drop_table - удалить таблицу | "
+              "create_DB - создать базу |"
+              "create_user - создать пользователя | "  # доработать
               "create_table - создать базу с таблицами \n "
+              "drop_DB - удалить базу | "
+              "drop_user - удалить пользователя | "  # доработать
+              "drop_table - удалить таблицу \n "
               "view_table - посмотреть таблицы | "
               "view_DB - посмотреть базы | "
-              "view_structure - посмотреть структуру таблицы \n"
+              "view_structure - посмотреть структуру таблицы \n "
               "view_role - роли пользователя | ",
-              "view_user - все пользователи \n",
+              "view_user - все пользователи | \n",
+              "grant_DB - предоставить права на базу | ",
+              "grant_table - предоставить права на таблицу \n",
               "-" * 130
               )
 
@@ -23,63 +30,63 @@ if __name__ == "__main__":
 
         if q == "create_DB":
             dtb = input("имя новой БД: ")
-            create_database(dtb)
+            self.dtbs.create_database(str(dtb))
             input()
+
         if q == "create_table":
             create_table()
             input()
 
         if q == "view_DB":
-            result = view_database()
+            result = self.dtbs.view_database()
             for row in result:
                 print(*row)
             input()
 
         if q == "drop_DB":
             dtb = input("name database: ")
-            result = drop_database(dtb)
-            input()
-
-        if q == "drop_table":
-            dtb = input("name database: ")
-            answer = view_table_database(dtb)
-            print(f"Таблицы в {dtb}")
-            for row in answer:
-                print(*row)
-            table = input("name table: ")
-            result = drop_table(dtb, table)
+            result = self.dtbs.drop_database(dtb)
             print(result)
             input()
 
+        if q == "drop_table":
+            dtb = input("name database: : ")
+            table = input("name table: ")
+            q = self.dtbs.view_table_database(dtb)
+            for rows in q:
+                if table not in rows:
+                    continue
+                else:
+                    result = self.dtbs.drop_table(dtb, table)
+                    print(result)
+                input()
+
         if q == "view_table":
             dtb = input("name database: ")
-            result = view_table_database(dtb)
-            if result is None:
-                print(f"{dtb} нет в списке")
-            else:
-                for row in result:
-                    print(*row)
-                input()
+            result = self.dtbs.view_table_database(dtb)
+            for rows in result:
+                print("таблица: ", *rows)
+            input()
 
         if q == "view_structure":
             dtb = input("name database: ")
             name_table = input("table name: ")
-            result = view_structure(dtb, name_table)
+            result = self.dtbs.view_structure(dtb, name_table)
             if result is None:
-                print("таблицы не найдены или база отсутствует")
-            else:
-                print("_" * 20)
-                print(f"database {dtb}, таблица {name_table}")
-                for row in result:
-                    print(row)
-                print("_" * 20)
+                print("Таблица не найдена")
+                return
+            print("_ " * 30)
+            print(f"database {dtb}, таблица {name_table}")
+            for rows in result:
+                print(rows)
+            print("_ " * 30)
             input()
 
         if q == "view_role":
-            dtb = input("ввести имя базы: ")
-            result = view_role(dtb)
+            result = self.dtbs.view_role()
             if result is None:
-                print("таблицы не найдены или база отсутствует")
+                print("База не найдена")
+                return
             else:
                 print("_" * 20)
                 print("Пользователи и их права: ")
@@ -96,7 +103,7 @@ if __name__ == "__main__":
             input()
 
         if q == "view_user":
-            result = view_user()
+            result = self.dtbs.view_user()
             if result is None:
                 print("нет доступа")
             else:
@@ -105,3 +112,48 @@ if __name__ == "__main__":
                     print(user[0])
                 print("*" * 30)
             input()
+
+        if q == "create_user":
+            user = input("Новый пользователь: ")
+            pswrd = input("Пароль нового пользователя: ")
+            result = self.dtbs.create_new_user(str(user), pswrd)
+            print(result)
+            input()
+
+        if q == "delete_user":
+            user = input("Удаляемый пользователь: ")
+            result = self.dtbs.delete_user(user)
+            print(result)
+            input()
+
+        if q == "grant_DB":
+            database_name = input("имя базы: ")
+            username = input("имя пользователя: ")
+            print("Привилегии", config.privilegy_DB)
+            privileges = input('Тип привилегий: ')
+            if privileges not in config.privilegy_DB:
+                print("некорректная привилегия")
+                return
+            else:
+                result = self.dtbs.grant_privileges(database_name, username, privileges)
+                print(result)
+            input()
+
+        if q == "grant_table":
+            username = input("имя пользователя: ")
+            dtb = input("имя базы: ")
+            table = input("название таблицы: ")
+            print("Привилегии", config.privilegy_table)
+            privileges = input('Тип привилегий: ')
+            if privileges not in config.privilegy_table:
+                print("некорректная привилегия")
+                return
+            else:
+                result = self.dtbs.grant_privileges_table(username, dtb, table, privileges)
+                print(result)
+            input()
+
+if __name__ == "__main__":
+    while True:
+        testObj = Operation()
+
